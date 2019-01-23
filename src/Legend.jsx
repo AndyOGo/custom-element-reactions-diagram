ac4c80b9e7cf812430596b9ca11bf90490a38915
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2019-01-23 09:48:35 +0100

    feat: added custom element types

--- src/Legend.jsx
+++ src/Legend.jsx
@@ -8,13 +8,13 @@ const Legend = ({ advanced }) => (advanced ? (
     <h2 className="hidden">
       Phases
     </h2>
-    <Phase name="Unknown" row={1} rowspan={2} docname="htmlunknownelement" docurl="https://html.spec.whatwg.org/multipage/dom.html">
+    <Phase name="Unknown" row={1} rowspan={4} docname="htmlunknownelement" docurl="https://html.spec.whatwg.org/multipage/dom.html">
       Still waiting for custom element definition.
     </Phase>
-    <Phase name="Upgrade" row={3} rowspan={1} docname="upgrades">
+    <Phase name="Upgrade" row={5} rowspan={1} docname="upgrades">
       Can not read the DOM.
     </Phase>
-    <Phase last name="Live" row={4} rowspan={2} docname="concept-collection-live" docurl="https://dom.spec.whatwg.org">
+    <Phase last name="Live" row={6} rowspan={2} docname="concept-collection-live" docurl="https://dom.spec.whatwg.org">
       Can work with DOM, run side effects, schedule updates.
     </Phase>
   </>
@@ -23,10 +23,13 @@ const Legend = ({ advanced }) => (advanced ? (
     <h2 className="hidden">
       Phases
     </h2>
-    <Phase name="Upgrade" row={1} rowspan={2}>
+    <Phase name="Unknown" row={1} rowspan={4} docname="htmlunknownelement" docurl="https://html.spec.whatwg.org/multipage/dom.html">
+      Still waiting for custom element definition.
+    </Phase>
+    <Phase name="Upgrade" row={5} rowspan={1} docname="upgrades">
       Can not read the DOM.
     </Phase>
-    <Phase last name="Live" row={3} rowspan={2}>
+    <Phase last name="Live" row={6} rowspan={2} docname="concept-collection-live" docurl="https://dom.spec.whatwg.org">
       Can work with DOM, run side effects, schedule updates.
     </Phase>
   </>
--- src/versions/v1/Connecting.jsx
+++ src/versions/v1/Connecting.jsx
@@ -8,13 +8,27 @@ import Initiator from '../../diagramElements/Initiator';
 
 const Connecting = ({ advanced }) => (advanced ? (
   <Section advanced name="Connecting" col={1} colspan={1}>
+      <Initiator
+        secondary
+        name="<autonomous-element>"
+        docname="custom-elements-autonomous-example"
+        row={1}
+        col={1}
+      />
+      <Initiator
+        secondary
+        name='<div is="built-in">'
+        docname="custom-elements-customized-builtin-example"
+        row={2}
+        col={1}
+      />
     <Arrow />
     <Method
       main
       name="define"
       docname="element-definition"
       type="unknown"
-      row={1}
+      row={3}
       col={1}
     />
     <Initiator
@@ -22,7 +36,7 @@ const Connecting = ({ advanced }) => (advanced ? (
       name="createElement()"
       docname="dom-document-createelement"
       docurl="https://dom.spec.whatwg.org"
-      row={2}
+      row={4}
     />
     <Arrow />
     <Method
@@ -30,7 +44,7 @@ const Connecting = ({ advanced }) => (advanced ? (
       name="constructor"
       docname="custom-element-constructor"
       type="upgrade"
-      row={3}
+      row={5}
       col={1}
     />
     <Initiator
@@ -38,7 +52,7 @@ const Connecting = ({ advanced }) => (advanced ? (
       name="appendChild()"
       docname="dom-node-appendchild"
       docurl="https://dom.spec.whatwg.org"
-      row={4}
+      row={6}
       col={1}
     />
     <Arrow solid />
@@ -47,7 +61,7 @@ const Connecting = ({ advanced }) => (advanced ? (
       name="connectedCallback"
       docname="custom-element-reactions:becomes-connected"
       type="live"
-      row={5}
+      row={7}
       colspan={1}
     />
     <Arrow withAlt solid />
@@ -55,64 +69,78 @@ const Connecting = ({ advanced }) => (advanced ? (
       main
       name="custom render"
       type="custom"
-      row={6}
+      row={8}
       col={1}
       colspan={5}
     />
   </Section>
 ) : (
   <Section advanced name="Connecting" col={1} colspan={1}>
-    <Arrow />
-    <Method
-      main
-      name="define"
-      docname="element-definition"
-      type="unknown"
-      row={1}
-      col={1}
-    />
-    <Initiator
-      secondary
-      name="createElement()"
-      docname="dom-document-createelement"
-      docurl="https://dom.spec.whatwg.org"
-      row={2}
-    />
-    <Arrow />
-    <Method
-      main
-      name="constructor"
-      docname="custom-element-constructor"
-      type="upgrade"
-      row={3}
-      col={1}
-    />
-    <Initiator
-      secondary
-      name="appendChild()"
-      docname="dom-node-appendchild"
-      docurl="https://dom.spec.whatwg.org"
-      row={4}
-      col={1}
-    />
-    <Arrow solid />
-    <Method
-      main
-      name="connectedCallback"
-      docname="custom-element-reactions:becomes-connected"
-      type="live"
-      row={5}
-      colspan={1}
-    />
-    <Arrow withAlt solid />
-    <Method
-      main
-      name="custom render"
-      type="custom"
-      row={6}
-      col={1}
-      colspan={5}
-    />
+      <Initiator
+        secondary
+        name="<autonomous-element>"
+        docname="custom-elements-autonomous-example"
+        row={1}
+        col={1}
+      />
+      <Initiator
+        secondary
+        name='<div is="built-in">'
+        docname="custom-elements-customized-builtin-example"
+        row={2}
+        col={1}
+      />
+      <Arrow />
+      <Method
+        main
+        name="define"
+        docname="element-definition"
+        type="unknown"
+        row={3}
+        col={1}
+      />
+      <Initiator
+        secondary
+        name="createElement()"
+        docname="dom-document-createelement"
+        docurl="https://dom.spec.whatwg.org"
+        row={4}
+      />
+      <Arrow />
+      <Method
+        main
+        name="constructor"
+        docname="custom-element-constructor"
+        type="upgrade"
+        row={5}
+        col={1}
+      />
+      <Initiator
+        secondary
+        name="appendChild()"
+        docname="dom-node-appendchild"
+        docurl="https://dom.spec.whatwg.org"
+        row={6}
+        col={1}
+      />
+      <Arrow solid />
+      <Method
+        main
+        name="connectedCallback"
+        docname="custom-element-reactions:becomes-connected"
+        type="live"
+        row={7}
+        colspan={1}
+      />
+      <Arrow withAlt solid />
+      <Method
+        main
+        name="custom render"
+        type="custom"
+        row={8}
+        col={1}
+        colspan={5}
+      />
   </Section>
 ));
 
