ec6f070cabca273804b2df116dbc2502c7d44ce9
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2019-02-20 21:17:16 +0100

    fix: don't use incomplete translations

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -5,19 +5,20 @@ export const defaultLocale = 'en-US';
 
 export const languageFiles = {
   'de-DE': import('./json/de-DE.json'),
-  'es-ES': import('./json/es-ES.json'),
-  'fr-FR': import('./json/fr-FR.json'),
-  'ja-JP': import('./json/ja-JP.json'),
-  'ko-KR': import('./json/ko-KR.json'),
-  'pl-PL': import('./json/pl-PL.json'),
-  'pt-BR': import('./json/pt-BR.json'),
-  'ru-RU': import('./json/ru-RU.json'),
-  'sq-AL': import('./json/sq-AL.json'),
-  'tr-TR': import('./json/tr-TR.json'),
-  'uk-UA': import('./json/uk-UA.json'),
-  'vn-VN': import('./json/vn-VN.json'),
-  'zh-CN': import('./json/zh-CN.json'),
-  'zh-TW': import('./json/zh-TW.json'),
+  // @todo: finish translations
+  // 'es-ES': import('./json/es-ES.json'),
+  // 'fr-FR': import('./json/fr-FR.json'),
+  // 'ja-JP': import('./json/ja-JP.json'),
+  // 'ko-KR': import('./json/ko-KR.json'),
+  // 'pl-PL': import('./json/pl-PL.json'),
+  // 'pt-BR': import('./json/pt-BR.json'),
+  // 'ru-RU': import('./json/ru-RU.json'),
+  // 'sq-AL': import('./json/sq-AL.json'),
+  // 'tr-TR': import('./json/tr-TR.json'),
+  // 'uk-UA': import('./json/uk-UA.json'),
+  // 'vn-VN': import('./json/vn-VN.json'),
+  // 'zh-CN': import('./json/zh-CN.json'),
+  // 'zh-TW': import('./json/zh-TW.json'),
 };
 
 export const supportedLocales = [
